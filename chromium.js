1859422aab7b2fe68e396b1224efb217be916281
anfdhepd7891 <50706640+anfdhepd7891@users.noreply.github.com>
2020-12-05 20:06:44 +0900

    Update chromium.js

--- chromium.js
+++ chromium.js
@@ -3,10 +3,10 @@ const puppeteer = require('puppeteer-core');
 
 async function getScreenshot(url, type, quality, fullPage, viewportWidth, viewportHeight) {
     const browser = await puppeteer.launch({
-        args: chrome.args,
+        args: ['--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A403 Safari/602.1'],
         executablePath: await chrome.executablePath,
         headless: chrome.headless,
-        args: ['--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A403 Safari/602.1'],
+       
         defaultViewport: {
             width: viewportWidth,
             height: viewportHeight
