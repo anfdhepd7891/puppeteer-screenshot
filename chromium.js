bf93fceb7259c44842f8cc9f51c8e6fc55f0c805
anfdhepd7891 <50706640+anfdhepd7891@users.noreply.github.com>
2020-12-07 15:22:46 +0900

    Update chromium.js

--- chromium.js
+++ chromium.js
@@ -16,7 +16,8 @@ async function getScreenshot(url, type, quality, fullPage, viewportWidth, viewpo
     const page = await browser.newPage();
        await page.setCacheEnabled(false);
      await page.setUserAgent('Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36');
-    await page.goto(url, {'timeout': 15000, 'waitUntil':'load'});
+    await page.goto(url);
+    await page.waitForNavigation();
     const file = await page.screenshot({ type,  quality, fullPage });
     await browser.close();
     return file;
